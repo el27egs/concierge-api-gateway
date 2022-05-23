261e7506756155a2f6acb639f0fc10389bd8d6d7
el27egs <el27egs@gmail.com>
2022-05-23 13:47:23 -0500

    Replacing system.out by log

--- src/main/java/com/ngineapps/concierge/apigateway/config/filters/AuthorizationHeaderFilter.java
+++ src/main/java/com/ngineapps/concierge/apigateway/config/filters/AuthorizationHeaderFilter.java
@@ -1,31 +1,40 @@
 package com.ngineapps.concierge.apigateway.config.filters;
 
 
+import com.nimbusds.jwt.JWTParser;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Component
+@Slf4j
 public class AuthorizationHeaderFilter implements GatewayFilterFactory<AuthorizationHeaderFilter.Config> {
 
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            System.out.println("First pre filter" + exchange.getRequest().getURI());
+            log.info("First pre filter" + exchange.getRequest().getURI());
 
             List<String> header = exchange.getRequest().getHeaders().getOrEmpty("Authorization");
 
             if (header.isEmpty()) {
+
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                 return exchange.getResponse().setComplete();
             }
+            log.debug("Authorization was " + header.get(0));
 
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                System.out.println("First post filter ");
+                log.info("First post filter ");
             }));
         };
     }
