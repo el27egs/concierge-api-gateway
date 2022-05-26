ee11acb06989cd68471f925bd30047bad7b552c9
el27egs <el27egs@gmail.com>
2022-05-25 21:16:33 -0500

    Adding spotless and applying format

--- src/main/java/com/ngineapps/concierge/apigateway/config/filters/AuthorizationHeaderFilter.java
+++ src/main/java/com/ngineapps/concierge/apigateway/config/filters/AuthorizationHeaderFilter.java
@@ -1,47 +1,59 @@
+/*
+ *     Copyright 2022-Present Ngine Apps @ http://www.ngingeapps.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ngineapps.concierge.apigateway.config.filters;
 
-
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @Component
 @Slf4j
-public class AuthorizationHeaderFilter implements GatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+public class AuthorizationHeaderFilter
+    implements GatewayFilterFactory<AuthorizationHeaderFilter.Config> {
 
-    @Override
-    public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> {
+  @Override
+  public GatewayFilter apply(Config config) {
+    return (exchange, chain) -> {
+      List<String> header = exchange.getRequest().getHeaders().getOrEmpty("Authorization");
 
-            List<String> header = exchange.getRequest().getHeaders().getOrEmpty("Authorization");
+      if (header.isEmpty()) {
 
-            if (header.isEmpty()) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+      }
 
-                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                return exchange.getResponse().setComplete();
-            }
+      log.info("Authorization header:  " + header.get(0).substring(0, 15) + "...");
 
-            log.info("Authorization header:  " + header.get(0).substring(0,15) + "...");
+      return chain.filter(exchange);
+    };
+  }
 
-            return chain.filter(exchange);
-        };
-    }
-    @Override
-    public Class<Config> getConfigClass() {
-        return Config.class;
-    }
+  @Override
+  public Class<Config> getConfigClass() {
+    return Config.class;
+  }
 
-    @Override
-    public Config newConfig() {
-        Config c = new Config();
-        return c;
-    }
+  @Override
+  public Config newConfig() {
+    Config c = new Config();
+    return c;
+  }
 
-    public static class Config {}
-
+  public static class Config {}
 }
--- src/main/java/com/ngineapps/concierge/apigateway/config/filters/LoggingGlobalFilter.java
+++ src/main/java/com/ngineapps/concierge/apigateway/config/filters/LoggingGlobalFilter.java
@@ -1,5 +1,27 @@
+/*
+ *     Copyright 2022-Present Ngine Apps @ http://www.ngingeapps.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ngineapps.concierge.apigateway.config.filters;
 
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ORIGINAL_REQUEST_URL_ATTR;
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -8,26 +30,22 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
-import java.util.Collections;
-import java.util.Set;
-
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ORIGINAL_REQUEST_URL_ATTR;
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
-
 @Slf4j
 @Component
 public class LoggingGlobalFilter implements GlobalFilter {
 
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        Set<URI> uris = exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
-        String originalUri = (uris.isEmpty()) ? "Unknown" : uris.iterator().next().toString();
-        Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
-        URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
-        log.info("Incoming request " + originalUri + " is routed to id: " + route.getId()
-                + ", uri:" + routeUri);
-        return chain.filter(exchange);
-    }
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    Set<URI> uris =
+        exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
+
+    String originalUri = (uris.isEmpty()) ? "Unknown" : uris.iterator().next().toString();
+    Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+    URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+
+    log.info(
+        "Incoming request {} is routed to id: {} , uri: ", originalUri, route.getId(), routeUri);
+
+    return chain.filter(exchange);
+  }
 }
